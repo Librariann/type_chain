02ed9e6a4545541a7a4c03fa65aa38c64a766d13
Librarian <okpc0306@naver.com>
2022-04-08 11:08:09 +0900

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const name = "Nicolas",
       age = 24,
-      gender = "male";
+      gender = "malee";
 
 const sayHi = (name: string, age: number, gender: string): void => {
   console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
